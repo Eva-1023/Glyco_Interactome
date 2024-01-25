595c5fc477cc496b4b46a3983ff67a4c64c2d851
shunyang-brightseed <sywang@ucdavis.edu>
2024-01-25 14:12:47 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,38 +3,35 @@ import streamlit.components.v1 as components
 import networkx as nx
 import matplotlib.pyplot as plt
 from pyvis.network import Network
-import got 
-import os 
+import got
+import os
+
+# Define a function to display the "Contact" tab content
 def contact_tab():
     st.title('Contact Us')
     st.write("You can reach out to us at contact@example.com")
-#Network(notebook=True)
-st.title('Glyco Interactome Network')
-# make Network show itself with repr_html
 
-#def net_repr_html(self):
-#  nodes, edges, height, width, options = self.get_network_data()
-#  html = self.template.render(height=height, width=width, nodes=nodes, edges=edges, options=options)
-#  return html
+# Define the main app
+def main():
+    st.title('Glyco Interactome Network')
 
-#Network._repr_html_ = net_repr_html
-st.sidebar.title('Choose your favorite Graph')
-path = 'data/html/'
-graph_set = set()
-for filename in os.listdir(path):
-    if filename.endswith(".html"):
-        graph_set.add(filename.replace('.html',''))
-option=st.sidebar.selectbox('select graph',(graph_set))
+    # Add your existing code for the network visualization here
 
+    st.sidebar.title('Choose your favorite Graph')
+    path = 'data/html/'
+    graph_set = set()
+    for filename in os.listdir(path):
+        if filename.endswith(".html"):
+            graph_set.add(filename.replace('.html', ''))
+    option = st.sidebar.selectbox('Select graph', (graph_set))
 
+    HtmlFile = open(path + option + '.html', 'r', encoding='utf-8')
+    source_code = HtmlFile.read()
+    components.html(source_code, height=1600, width=1000)
 
+    # Add a "Contact" tab to the sidebar
+    if st.sidebar.button('Contact'):
+        contact_tab()
 
-
-HtmlFile = open(path+option+'.html', 'r', encoding='utf-8')
-source_code = HtmlFile.read() 
-components.html(source_code, height = 1600,width=1000)
-
-
-# Add a "Contact" tab to the sidebar
-if st.sidebar.button('Contact'):
-    contact_tab()
+if __name__ == "__main__":
+    main()
