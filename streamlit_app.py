acb708e84d6d71439f73076d474371f7fa079f1f
shunyang-brightseed <sywang@ucdavis.edu>
2024-01-25 14:16:06 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,22 +1,15 @@
 import streamlit as st
-import streamlit.components.v1 as components
 import networkx as nx
 import matplotlib.pyplot as plt
 from pyvis.network import Network
 import got
 import os
 
-# Define a function to display the "Contact" tab content
-def contact_tab():
-    st.title('Contact Us')
-    st.write("You can reach out to us at contact@example.com")
-
-# Define the main app
-def main():
+# Define a function to display the network visualization
+def display_network():
     st.title('Glyco Interactome Network')
 
     # Add your existing code for the network visualization here
-
     st.sidebar.title('Choose your favorite Graph')
     path = 'data/html/'
     graph_set = set()
@@ -27,12 +20,22 @@ def main():
 
     HtmlFile = open(path + option + '.html', 'r', encoding='utf-8')
     source_code = HtmlFile.read()
-    components.html(source_code, height=1600, width=1000)
+    st.components.v1.html(source_code, height=1600, width=1000)
 
-    # Add a "Contact" button to open in a new tab
-    contact_link = '<a href="#" target="_blank">Contact</a>'
-    if st.sidebar.button(contact_link, on_click=contact_tab):
-        pass  # You can perform an action here if needed
+# Define a function to display the "Contact" tab content
+def contact_tab():
+    st.title('Contact Us')
+    st.write("You can reach out to us at contact@example.com")
+
+# Define the main app
+def main():
+    st.sidebar.title('Navigation')
+    app_mode = st.sidebar.radio("Go to", ('Network Graph', 'Contact'))
+
+    if app_mode == 'Network Graph':
+        display_network()
+    elif app_mode == 'Contact':
+        contact_tab()
 
 if __name__ == "__main__":
     main()
